70d7ee264efcb19f0205095ebffecb623865be6d
Kis Balazs <Balazs.Kis@student.utcluj.ro>
2021-08-08 19:50:19 +0300

    initial commit

---
+++ image_processing.py
@@ -0,0 +1,97 @@
+from configparser import ConfigParser
+
+import numpy as np
+from PIL import Image
+from collections import deque
+import matplotlib.pyplot as plt
+
+# tesseract setup tutorial: https://www.youtube.com/watch?v=4DrCIVS5U3Y
+import pytesseract
+
+
+class GameCanvasQueue():
+	"""data structure class holding the last 3 canvas images from the game"""
+	def __init__(self, max_len=3):
+		# queue keeping only the last <max_len> elements => short term memory
+		self.q = deque(
+			maxlen=max_len
+		)
+		
+	def append(self, val: Image) -> None:
+		"""append an image to the queue while keeping the constant length based on the queue data strucutre
+		
+		:param val: an image object
+		:return: None"""
+		self.q.append(val)
+
+
+class CanvasProcessing():
+	"""class for working with the IP part of the canvas image in order to extract information from
+	the game"""
+	def __init__(self):
+		config = ConfigParser()
+		config.read('env_config.ini')
+
+		pytesseract.pytesseract.tesseract_cmd = config['ENV']['tesseract_path']
+	
+	def __extract_region(self, canvas: Image, roi: tuple) -> Image:
+		"""function to crop the canvas image in the given region of interest in the format
+		(top-left_X, top-left_Y, bottom-right_X, bottom-right_Y) and return items
+		
+		:param canvas: Image of the game
+		:param roi: tuple giving the region of interest
+		:return: cropped region of interest"""
+		# CanvasProcessing.visualize(canvas.crop(roi))
+		return canvas.crop(
+			roi
+		)
+	
+	def extract_score(self, canvas: Image) -> int:
+		"""function to use the region extraction and OCR\tesseract in order to extract the
+		player score as a number, and if not possible, return 0
+		
+		:param canvas: Image of the game
+		:return: integer score"""
+		score_roi = self.__extract_region(
+			canvas,
+			roi=(530, 5, 590, 25)
+		)
+		score_text = pytesseract.image_to_string(
+			score_roi,
+			lang='eng'
+		)
+		
+		try:
+			score = int(score_text)
+		except:
+			score = 0
+		return score
+		
+	def is_game_over(self, canvas: Image) -> bool:
+		"""function to use the region extraction and OCR\tesseract in order to determine
+		whether the game over screen is present by checking a desired region for game over
+		text and looking for at least half of the letters present in the text from the region
+		
+		:param canvas: Image of the game
+		:return: truth value of the game over screen being presented on the canvas"""
+		game_over_roi = self.__extract_region(
+			canvas,
+			roi=(200, 40, 400, 55)
+		)
+		game_over_text = pytesseract.image_to_string(
+			game_over_roi,
+			lang='eng',
+			config='--psm 10 --oem 3 -c tessedit_char_whitelist=GAMEOVER'
+		)
+		# print(game_over_text)
+		GO = 'GAMEOVER'
+		return sum([ch in game_over_text for ch in list(GO)]) > len(GO) // 2
+	
+	@staticmethod
+	def visualize(img: Image):
+		"""helper function to show the given image using matplotlib.pyplot
+		
+		:param img: any image
+		:return: None"""
+		plt.imshow(img)
+		plt.show()
---
+++ player.py
@@ -0,0 +1,86 @@
+import time
+import enum
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class ActionType(enum.Enum):
+	JUMP = 1
+	DUCK = 2
+	SHORT_JUMP = 3
+
+
+class PlayerAgent():
+	"""class for keeping the canvas element and perform actions and sequences of actions
+	on it based on stimulus from the IP and game environment"""
+	def __init__(self, driver):
+		self.driver = driver
+	
+	def hold_key(self, key: Keys, duration: int) -> None:
+		"""function to hold down a specific key until the duration (in seconds) is not elapsed
+		
+		:param key: Keys type object from selenium
+		:param duration: time in seconds
+		:return None"""
+		kd = ActionChains(self.driver).key_down(key)
+		ku = ActionChains(self.driver).key_up(key)
+
+		endtime = time.time() + duration
+		while True:
+			kd.perform()
+
+			if time.time() > endtime:
+				ku.perform()
+				return
+	
+	def __jump(self, duration: int) -> None:
+		"""function to call the hold_key function for the ARROW_UP key for given amount of time
+		which translates in a duck in the game
+		
+		:param duration: duration in seconds
+		:return: None"""
+		self.hold_key(
+			key=Keys.ARROW_UP,
+			duration=duration
+		)
+		
+	def __duck(self, duration: int) -> None:
+		"""function to call the hold_key function for the ARROW_DOWN key for given amount of time
+		which translates in a duck in the game
+		
+		:param duration: duration in seconds
+		:return: None"""
+		self.hold_key(
+			key=Keys.ARROW_DOWN,
+			duration=duration
+		)
+
+	def action_sequence(self, action_type: ActionType, duration=0.1) -> None:
+		"""public function to be used for the agent to interact with the game based on the input from
+		the image processing step
+		
+		:param action_type: enum which gives action options to the agent
+		:param duration: duration in seconds
+		:return: None"""
+		if action_type == ActionType.JUMP:
+			self.__jump(
+				duration=duration
+			)
+		elif action_type == ActionType.DUCK:
+			self.__duck(
+				duration=duration
+			)
+		elif action_type == ActionType.SHORT_JUMP:
+			self.__jump(
+				duration=duration
+			)
+			self.hold_key(
+				key=Keys.NULL,
+				duration=0.05
+			)
+			self.__duck(
+				duration=duration
+			)
+		else:
+			pass
